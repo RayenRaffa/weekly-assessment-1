75cc6c822845f9f760582820b5b623de2ee6a5a9
siwarhkimi <cywarhkimi@gmail.com>
2020-03-16 10:01:28 +0100

    finished

--- queueFromStacks.js
+++ queueFromStacks.js
@@ -24,5 +24,18 @@ class Stack {
 ////////////////////////////////////////////////////////////
 
 class Queue {
+  constructor() {
+    this.data = [];
+  }
+  enqueue(record) {
+    this.data.unshift(record);
+  }
 
+  dequeue() {
+    return this.data.shift();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
 }
