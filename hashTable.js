15278a3dd4a80e82125f8c520536d987a9df6a3d
taha0505 <taha.khelfa@hotmail.fr>
2020-03-16 10:54:38 +0100

    assessment I

--- hashTable.js
+++ hashTable.js
@@ -1,17 +1,28 @@
-// implement your hashTable data structure
+/ implement your hashTable data structure
 
 var HashTable = function() {
   this._storage = [];
   this._count = 0;
   this._limit = 8;
+  for (let i = 0; i < this.storage.length; i++) {
+    this.storage[i] = new Map();
+  
 }
 
 
 HashTable.prototype.insert = function(key, value) {
+  var index = hash(key, this._limit);
+    this.storage[ this._count].set(key, value);
+  
 };
 
 
 HashTable.prototype.remove = function(key) {
+  var index = hash(key, this._limit);
+    var temp = this.search(key);
+    this.storage[ this._count].delete(key);
+    return temp;
+  
 };
 
 
@@ -38,3 +49,5 @@ HashTable.prototype.resize = function(newLimit) {
 HashTable.prototype.retrieveAll = function() {
   return this._storage;
 };
+
+  
