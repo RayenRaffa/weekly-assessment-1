238eade7fbe1022680cea4c9ad872575498fd6bb
raffa <raffa.rayen@gmail.com>
2020-03-17 20:40:27 +0100

    add initial impl of hash table

--- hashTable.js
+++ hashTable.js
@@ -8,15 +8,50 @@ var HashTable = function() {
 
 
 HashTable.prototype.insert = function(key, value) {
+  var index  = this.hashFunc(key, this._limit);
+  var tuple  = [key, value];
+  var bucket = this._storage[index];
+  if (bucket) {
+    bucket.push(tuple);
+  } else {
+    bucket = [tuple];
+  }
+  this._storage[index] = bucket;
+  this._count++;
+  if (this._count/this._limit > 0.75) {
+    this.resize(this._limit*2);
+  }
 };
 
 
-HashTable.prototype.remove = function(key) {
+HashTable.prototype.remove = function(key) { 
+  var index  = this.hashFunc(key, this._limit);
+  var bucket = this._storage[index];
+  for (var i=0; i<bucket.length; i++) {
+    if (bucket[i][0] === key) {
+      value = bucket[i][1];
+      bucket.splice(i,1);
+      this._storage[index] = bucket;
+      this._count--;
+    }
+  }
+  if ((this._count/this._limit < 0.25) && (this._limit > 2)) {
+    this.resize(Math.floor(this._limit/2));
+  }
+  return value;
 };
 
 
 
-HashTable.prototype.retrieve = function(key) {
+HashTable.prototype.retrieve = function(key) { 
+  var index  = this.hashFunc(key, this._limit);
+  var bucket = this._storage[index];
+  for (var i=0; i<bucket.length; i++) {
+    if (bucket[i][0] === key) {
+      return bucket[i][1];
+    }
+  }
+  return undefined;
 };
 
 
@@ -32,9 +67,26 @@ HashTable.prototype.hashFunc = function(str, max) {
 
 
 HashTable.prototype.resize = function(newLimit) {
+  var newHashTable = new HashTable();
+  newHashTable._limit = newLimit;
+  for (var i=0; i<this._limit; i++) {
+    bucket = this._storage[i];
+    if (bucket) {  
+      for (var j=0; j<bucket.length; j++) {
+        tuple = bucket[j];
+        key   = tuple[0];
+        value = tuple[1];
+        newHashTable.insert(key,value);
+      }
+    }
+  }
+  this._limit   = newLimit;
+  this._count   = newHashTable._count;
+  this._storage = newHashTable._storage;
 };
 
 
 HashTable.prototype.retrieveAll = function() {
   return this._storage;
 };
+
